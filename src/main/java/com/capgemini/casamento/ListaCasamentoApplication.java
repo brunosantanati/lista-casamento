26a1a12d2c8c4e9db587d1f6519f71e1959eee52
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2020-05-16 17:58:15 -0300

    Primeiro commit. Arquivos do projeto recem criado.

---
+++ src/main/java/com/capgemini/casamento/ListaCasamentoApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.casamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaCasamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaCasamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/casamento/ListaCasamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.casamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaCasamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
